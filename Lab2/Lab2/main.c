c5fb8336d46f84f6d19aff20f90d400d3ae89a81
Zuguang Liu <liu2z2@mail.uc.edu>
2020-02-10 16:37:52 -0500

    Lab2 finished

--- Lab2/Lab2/main.c
+++ Lab2/Lab2/main.c
@@ -13,19 +13,37 @@ PD0-7 : Flip switches
 PB0-1 : Digit0-1 select
 PB2   : Segment A
 PC5-0 : Segment B-G
-PB5   : On-board LED
 */
 
-volatile int seg_bin;
+volatile int seg_sel; //Flag to select two 7 segs
+volatile int seg_bin; //7 bit var to store 7 seg on/off values
 int bin_to_segs (int);
 
+//Interrupt to toggle two 7 seg selects b/w 01 and 10
 ISR(TIMER1_COMPA_vect)
 {
-	cli();
-	if (PORTB0) seg_bin = PIND & 0b00001111; //Lower 4 bits
-	else seg_bin = PIND >> 4 & 0b00001111; //Upper 4 bits
-	PORTB ^= (1<<PORTB0); //Toggle PB0, PB1 auto toggle on interrupt
-	sei();
+	if (seg_sel) {
+		PORTB |= (1 << PORTB0); //Deselect first 7 seg
+		PORTB &= ~(1 << PORTB1); //Select second 7 seg
+		seg_bin = bin_to_segs( (~PIND) & 0b1111 );
+		//PORTC = (~PIND) & 0b111111;
+	}
+	else {
+		PORTB &= ~(1 << PORTB0); //Select first 7 seg
+		PORTB |= (1 << PORTB1); //Deselect second 7 seg
+		seg_bin = bin_to_segs( (~PIND >> 4) & 0b1111 );
+		//PORTC = (~PIND) & 0b111111;
+	}
+	
+	//Turn on/off seg A
+	if (seg_bin >> 6) PORTB |= (1 << PORTB2);
+	else PORTB &= ~(1 << PORTB2);
+	
+	//Turn on/off seg B-G
+	PORTC = seg_bin & 0b111111;
+	
+	//Toggle flag
+	seg_sel ^= 1;
 }
 
 int main(void)
@@ -34,32 +52,28 @@ int main(void)
 	DDRD = 0;
 	DDRB = 0b00000111;
 	DDRC = 0b00111111;
+	
+
 	//Timer and interrupt config
 	cli();
-	TCCR1A = (0<<COM1A1)|(1<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<WGM11)|(0<<WGM10); //Timer1 CTC by toggling PB1
+	TCCR1A = (0<<COM1A1)|(0<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<WGM11)|(0<<WGM10); //Timer1 CTC
 	TCCR1B = (0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(1<<WGM12)|(1<<CS12)|(0<<CS11)|(0<<CS10); //Timer1 scaled by 256
 	TIMSK1 = (0<<ICIE1)|(0<<OCIE1B)|(1<<OCIE1A)|(0<<TOIE1); //Enable match interrupt
-	OCR1A = 62500; //1 sec
+	//OCR1A = 62500; //1 sec
 	//OCR1A = 15625; //250 ms
 	//OCR1A = 6250;  //100 ms
-	//OCR1A = 1875; //30 ms
-	PORTB = (1<<PORTB0);
+	OCR1A = 1875; //30 ms
+	//OCR1A = 625; //10 ms
 	sei(); //Enable global interrupt
 	
 	
     while (1) 
     {
-		PORTC |= bin_to_segs(seg_bin) & 0b111111;
-		PORTB |= bin_to_segs(seg_bin) >> 4 & 0b0000100;
-		/*
-		7segs: ABCDEFG
-		PORTC: 6543210
-		PORTB: XXXXABC
-		*/
+		
     }
 }
 
-int bin_to_segs (int bin_value) //Converts bin value from switches to 4 bit A-G segs
+int bin_to_segs (int bin_value) //Converts 4 bit value from switches to 7 bit A-G segs
 {
 	switch (bin_value)
 	{
@@ -88,7 +102,7 @@ int bin_to_segs (int bin_value) //Converts bin value from switches to 4 bit A-G 
 			return 0b1110000;
 			break;
 		case 0x8:
-			return 0b0000000;
+			return 0b1111111;
 			break;
 		case 0x9:
 			return 0b1110011;
